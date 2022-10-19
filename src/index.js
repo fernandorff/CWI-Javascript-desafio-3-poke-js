54b05da3f975c4d96312548c1d89b967b935de4c
Fernando Rocha <fernandorff92@gmail.com>
2022-10-19 20:40:27 -0300

    finalizando

--- src/index.js
+++ src/index.js
@@ -1,1 +1,51 @@
-// inicie por aqui
+import pokemons from "./pokemons.json" assert { type: "json" };
+
+let pokemonsLivres = pokemons;
+
+function criarTreinador(inputNome, inputIdade, inputPokemonInicial) {
+  const treinador = {
+    nome: inputNome,
+    idade: inputIdade,
+    pokemonInicial: inputPokemonInicial,
+    listaPokemons: [inputPokemonInicial],
+  };
+  return treinador;
+}
+
+function capturarPokemon(treinador, pokemon) {
+  const capturado = pokemon;
+
+  treinador.listaPokemons.push(capturado);
+
+  subirLevel(treinador.listaPokemons);
+}
+
+function subirLevel(lista) {
+  lista.forEach((element) => {
+    element.level += 1;
+    evoluir(element, lista, pokemonsLivres);
+  });
+}
+
+function evoluir(element, lista) {
+  if (element.evolucao !== null) {
+    if (element.level == element.evolucao.level) {
+      let evoluido = {
+        ...lista[element.evolucao.id - 1],
+        level: element.evolucao.level,
+      };
+      console.log(evoluido);
+      element = evoluido;
+    }
+  }
+}
+
+const ash = criarTreinador("Ash Ketchum", 12, pokemonsLivres[0]);
+
+capturarPokemon(ash, pokemonsLivres[3]);
+capturarPokemon(ash, pokemonsLivres[6]);
+capturarPokemon(ash, pokemonsLivres[9]);
+capturarPokemon(ash, pokemonsLivres[12]);
+capturarPokemon(ash, pokemonsLivres[14]);
+
+console.log(ash.listaPokemons);
